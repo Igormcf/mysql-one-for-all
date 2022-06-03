64ded709a49bbdead3e361523168328cb79add43
Igormcf <igormaciel82@gmail.com>
2022-06-03 18:55:57 -0300

    Corrige erro desafio 06

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,7 @@
 SELECT
-	ROUND(MIN(p.value), 2) AS faturamento_minimo,
   ROUND(MAX(p.value), 2) AS faturamento_maximo,
   ROUND(AVG(p.value), 2) AS faturamento_medio,
+	ROUND(MIN(p.value), 2) AS faturamento_minimo,
   ROUND(SUM(p.value), 2) AS faturamento_total
 FROM SpotifyClone.plan AS p
 INNER JOIN SpotifyClone.users AS u
