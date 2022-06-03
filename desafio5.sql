313f77cefcc3b030d4b8b3af30c799f3ef4095cb
Igormcf <igormaciel82@gmail.com>
2022-06-03 17:56:35 -0300

    Corrige erro desafio 05

--- desafio5.sql
+++ desafio5.sql
@@ -1,9 +1,9 @@
 SELECT
 	s.name AS cancao,
-  COUNT(h.user_id) AS reproducoes
+    COUNT(h.user_id) AS reproducoes
 FROM SpotifyClone.song AS s
 INNER JOIN SpotifyClone.history AS h
 ON s.song_id = h.song_id
 GROUP BY cancao
-ORDER BY reproducoes DESC
+ORDER BY reproducoes DESC, cancao ASC
 LIMIT 2;
