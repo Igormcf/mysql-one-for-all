fe1047dda0398cab880f8305e78899608e1082e1
Igormcf <igormaciel82@gmail.com>
2022-06-03 17:54:05 -0300

    Finaliza desafio 05

---
+++ desafio5.sql
@@ -0,0 +1,9 @@
+SELECT
+	s.name AS cancao,
+  COUNT(h.user_id) AS reproducoes
+FROM SpotifyClone.song AS s
+INNER JOIN SpotifyClone.history AS h
+ON s.song_id = h.song_id
+GROUP BY cancao
+ORDER BY reproducoes DESC
+LIMIT 2;
