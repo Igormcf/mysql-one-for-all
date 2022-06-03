b0f02a4fec7e530fbcf668902533f13063a0f19f
Igormcf <igormaciel82@gmail.com>
2022-06-03 18:12:19 -0300

    Finaliza desafio 06

---
+++ desafio6.sql
@@ -0,0 +1,8 @@
+SELECT
+	ROUND(MIN(p.value), 2) AS faturamento_minimo,
+  ROUND(MAX(p.value), 2) AS faturamento_maximo,
+  ROUND(AVG(p.value), 2) AS faturamento_medio,
+  ROUND(SUM(p.value), 2) AS faturamento_total
+FROM SpotifyClone.plan AS p
+INNER JOIN SpotifyClone.users AS u
+ON p.plan_id = u.plan_id;
